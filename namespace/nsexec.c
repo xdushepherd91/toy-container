9bf6c82fc4a2569e9be88c183e16cd0af77cddab
xdushepherd <874654098@qq.com>
2019-11-18 10:23:40 +0800

    添加c库引用头文件

--- namespace/nsexec.c
+++ namespace/nsexec.c
@@ -1,17 +1,16 @@
-##include
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+static char child_stack[STACK_SIZE];
 
 int flag = CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWUTS |CLONE_NEWCGROUP;
 
-/*
-    xdushepherd 2019/11/18 10:04
-    复制runc中的代码，用来作为clone函数的第二参数
- */
-struct clone_t {
-
-	char stack[4096] __attribute__ ((aligned(16)));
-	char stack_ptr[0];
-
-};
 
 void child_func(void){
     sethostname("In Namespace", 12);
@@ -25,9 +24,8 @@ void nsexec(void){
        1. clone子进程，使得子进程可以进入新的命名空间
     */
 
-    struct clone_t cstack;
 
-    int child_pid = clone(child_func,cstack.stack_ptr,flag);
+    int child_pid = clone(child_func,child_stack+STACK_SIZE,flag);
 
 
 
