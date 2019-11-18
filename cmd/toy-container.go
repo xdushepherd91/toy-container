bb880d0ebf7c10e4efd51f25022a18e0ae76ceb5
xdushepherd <874654098@qq.com>
2019-11-18 14:44:21 +0800

    使用urfave/cli

--- cmd/toy-container.go
+++ cmd/toy-container.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
+	"github.com/urfave/cli"
 	"os"
 )
 
 var (
-	toyContainer = &cobra.Command{
-		Use:   "toy-contianer",
-		Short: "一个用来创建玩具容器的命令行工具",
-		Long: "toy-container是一个仿照runc的轮子，可以讲一组程序运行在特定的namespace和cgroup之中，实现资源的隔离",
-	}
+	toyContainer = cli.NewApp()
 )
 
 func init() {
@@ -19,14 +15,16 @@ func init() {
 
 func main() {
 
+	toyContainer.Usage = "toy-container"
+	toyContainer.Name = "toy-container"
 
-	toyContainer.AddCommand(initCommand)
-	toyContainer.AddCommand(runCommand)
-	println("toy-container enter")
-	if err := toyContainer.Execute(); err != nil {
-		println("toy-container命令执行出错")
-		os.Exit(1)
+	toyContainer.Commands =[]cli.Command{
+		initCommand,
+		runCommand,
 	}
 
 
+	if err := toyContainer.Run(os.Args); err != nil {
+		os.Exit(1)
+	}
 }
