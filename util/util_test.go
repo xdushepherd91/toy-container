5d30408a6a5bea35caa09173d930c6ec6b9a9040
xdushepherd <874654098@qq.com>
2019-11-20 10:45:11 +0800

    编写pid文件数据读取和解析相关函数

--- util/util_test.go
+++ util/util_test.go
@@ -48,3 +48,24 @@ func Test_getPidFromFile(t *testing.T) {
 		})
 	}
 }
+
+func TestFileExists(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"files",args{"D:/test.txt"},true},
+		{"not exist",args{"D:/test.pid"},false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FileExists(tt.args.path); got != tt.want {
+				t.Errorf("FileExists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
